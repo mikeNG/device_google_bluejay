9235544042e97cb88f462700e50b0298506295a7
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:11:38 +0300

    [WIP] calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 7c6f7dd7ba4157308b958f37bcb4eea008051590)

--- calyx_bluejay.mk
+++ calyx_bluejay.mk
@@ -11,6 +11,12 @@ PRODUCT_NAME := calyx_bluejay
 PRODUCT_MODEL := Pixel 6a
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bluejay \
+    PRIVATE_BUILD_DESC="bluejay-user 12 SQ1D.220205.004 8151327 release-keys"
+
+BUILD_FINGERPRINT := google/bluejay/bluejay:12/SQ1D.220205.004/8151327:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
