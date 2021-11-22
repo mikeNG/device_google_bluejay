20f63ac6f491f3f882a1bb6bed46336f19e4f56f
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit bc88fcd262e70cac89223b460611b2fc444d4b13
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:25:25 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: If3f761b8178b1663b73eb705d77105bd30ec15a9
        (cherry picked from commit 8d48d9ea51645e468a015465b919c365ecb7da90)
    
    commit a3196e48fe1795cc6acbe37ff847eec6ebc13a9c
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:43:51 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08
        (cherry picked from commit 7266f839ce08df3aa61cc80f28afff3c0d3f77ac)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ calyx_bluejay.mk
@@ -0,0 +1,11 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bluejay/device-calyx.mk)
+$(call inherit-product, device/google/bluejay/aosp_bluejay.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_bluejay
+PRODUCT_MODEL := Pixel 6a
+PRODUCT_RESTRICT_VENDOR_FILES := false
