CalyxOS/device_google_raviole

Add device manifest for radio
Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

device-calyx.mk
@@ -14,6 +14,10 @@ TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
 
 # Build necessary packages for vendor
 
+#
+DEVICE_MANIFEST_FILE += \
+    device/google/raviole/manifest_radio.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \

mikeNG/device_google_bluejay

device-calyx.mk
@@ -11,3 +11,7 @@ PRODUCT_COPY_FILES += \
     device/google/bluejay/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
 TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
+
+# Radio
+DEVICE_MANIFEST_FILE += \
+    device/google/bluejay/manifest_radio.xml